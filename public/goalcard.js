471e08576379007db3f2d8d69d856a2887b71519
DJA0616 <b2029djfasis@pshs.edu.ph>
2025-12-12 22:57:14 +0800

    Changed FIle Paths

--- public/goalcard.js
+++ public/goalcard.js
@@ -7,7 +7,8 @@ for (let i = 0; i < goalCounter; i++) {
     const clone = template.content.cloneNode(true);
     const img = clone.querySelector('.plant-image') || clone.querySelector('#plant');
     if (img) {
-        img.src = `../assets/plants/plant-${(i % 5) + 1}.png`;
+        // paths should be relative to the HTML document (repo root), not the JS file
+        img.src = `assets/plants/plant-${(i % 5) + 1}.png`;
     }
     goalCardDisplay.appendChild(clone);
 }
