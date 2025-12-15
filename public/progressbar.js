97707b14ac11df98fa5c3e5b8bfd6f390ba1cb8c
DJA1016 <b2029djfasis@pshs.edu.ph>
2025-12-15 13:31:58 +0800

    Improve main UI

--- public/progressbar.js
+++ public/progressbar.js
@@ -1,3 +1,4 @@
+const progressDisplay = document.getElementById("progress-display");
 const progressBar = document.getElementById("progress-bar");
 const progressCounter = document.getElementById("progress-counter");
 
@@ -5,3 +6,16 @@ var progress = 67;
 
 progressBar.style.width = String(30 * progress/100) + "vw";
 progressCounter.textContent = progress + "%";
+
+function onHover() {
+    progressBar.style.width = "100%";
+    progressCounter.textContent = "Go";
+}
+
+function onLeave() {
+    progressBar.style.width = String(30 * progress/100) + "vw";
+    progressCounter.textContent = progress + "%";
+}
+
+progressDisplay.addEventListener("mouseover", onHover);
+progressDisplay.addEventListener("mouseout", onLeave);
